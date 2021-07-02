7a22ee2f500aba4b061070e49fca843a8c13ed85
barrytrearty <btrearty@gmail.com>
2021-07-02 13:48:36 +0100

    created a function for displaying questions

--- quiz.js
+++ quiz.js
@@ -1,12 +1,21 @@
 // Use this to prevent generating same question twice
-const generateUniqueNumber = function (array) {
-  let randomNumber = Math.floor(Math.random() * 76 + 1);
+const generateUniqueNumber = function (num, array) {
+  let randomNumber = Math.floor(Math.random() * num);
   while (array.includes(randomNumber)) {
-    randomNumber = Math.floor(Math.random() * 76 + 1);
+    randomNumber = Math.floor(Math.random() * num);
   }
   array.push(randomNumber);
   return randomNumber;
 };
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
 
 const welcomePage = document.getElementById("welcome-page");
 const body = document.getElementsByTagName("body")[0];
@@ -22,8 +31,20 @@ const guatemalaQ = document.getElementById("guatemalaQ");
 const nigeriaQ = document.getElementById("nigeriaQ");
 const gameOver = document.getElementById("game-over");
 const seeScoreButton = document.getElementById("see-score");
+const questionContainer = document.getElementById("grid-questions-container");
 
 let score = 0;
+
+// const getCurrentlySelectedQuestion = function () {
+//   return document.querySelector(".selected");
+// };
+
+// const selectedQuestion = document.getElementsByClassName("selected")[]
+
+// const currentlySelectedQuestion = getCurrentlySelectedPhone() // Looking for any "selected" day in the page
+//   if (currentlySelectedPhoneNode !== null) { // If there is any selected day...
+//       currentlySelectedPhoneNode.classList.remove("selected") // ...we remove the "selected" class from it
+//   }
 
 const displayFirstQuestion = function () {
   welcomePage.remove();
@@ -131,15 +152,148 @@ const increaseScore = function () {
   console.log(score);
 };
 
-// const questions = {
+const displayQuestion = function (object) {
+  let question = document.createElement("section");
+
+  question.innerHTML = `<h3 class="question">${object.question}</h3>`;
+  question.classList.add("question-container");
+
+  questionContainer.appendChild(question);
+
+  let possibleAnswers = object.incorrect_answers;
+  let correctAnswer = object.correct_answer;
+  possibleAnswers.push(correctAnswer);
+  shuffleArray(possibleAnswers);
+  console.log(possibleAnswers);
+
+  for (i = 0; i < possibleAnswers.length; i++) {
+    let newAnswer = document.createElement("button");
+    newAnswer.innerText = possibleAnswers[i];
+    newAnswer.classList.add("answer");
+    if (possibleAnswers[i] == correctAnswer) {
+      newAnswer.classList.add("correct");
+    }
+    questionContainer.appendChild(newAnswer);
+    console.log(newAnswer);
+  }
+};
+
+const questions = [
+  {
+    category: "Geography",
+    type: "multiple",
+    difficulty: "medium",
+    question: "What is the Capital of Ireland?",
+    correct_answer: "Dublin",
+    incorrect_answers: ["Belfast", "Glasgow", "Edinburgh"],
+  },
+];
+
+displayQuestion(questions[0]);
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "easy",
+//     question: "What is the Capital of Belgium?",
+//     correct_answer: "Brussells",
+//     incorrect_answers: [
+//       "Bruges",
+//       "Ghent",
+//       "Belgium City",
+//     ]
+//   },
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "easy",
+//     question: "What is the Capital of France?",
+//     correct_answer: "Paris",
+//     incorrect_answers: [
+//       "Marseilles",
+//       "Lyon",
+//       "France City",
+//     ]
+//   },
+// {
 //     category: "Geography",
 //     type: "multiple",
 //     difficulty: "easy",
 //     question: "What is the Capital of Spain?",
-//     correct_answer: "Dublin",
+//     correct_answer: "Madrid",
 //     incorrect_answers: [
-//       "Cork",
-//       "Limerick",
-//       "Galway",
+//       "Valencia",
+//       "Barcelona",
+//       "Spain",
+//     ]
+//   },
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "easy",
+//     question: "What is the Capital of Italy?",
+//     correct_answer: "Rome",
+//     incorrect_answers: [
+//       "Milan",
+//       "Naples",
+//       "Italy City",
+//     ]
+//   },
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "medium",
+//     question: "What is the Capital of Romania?",
+//     correct_answer: "Bucharest",
+//     incorrect_answers: [
+//       "Budapest",
+//       "Brașov",
+//       "Romania City",
+//     ]
+//   },
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "difficult",
+//     question: "What is the Capital of Cameroon?",
+//     correct_answer: "Yaoundé",
+//     incorrect_answers: [
+//       "Bamenda",
+//       "Douala",
+//       "Cameroon City",
+//     ]
+//   },
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "medium",
+//     question: "What is the Capital of Peru?",
+//     correct_answer: "Lima",
+//     incorrect_answers: [
+//       "Bogota",
+//       "La Paz",
+//       "Peru City",
+//     ]
+//   },
+// {
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "difficult",
+//     question: "What is the Capital of Guatamala?",
+//     correct_answer: "Guatamala City",
+//     incorrect_answers: [
+//       "Quetzaltenango",
+//       "Antigua",
+//       "San Juan",
+//     ]
+//   },
+//     category: "Geography",
+//     type: "multiple",
+//     difficulty: "medium",
+//     question: "What is the Capital of Nigeria?",
+//     correct_answer: "Abuja",
+//     incorrect_answers: [
+//       "Lagos",
+//       "Ibadan",
+//       "Benin City",
 //     ]
 //   },
